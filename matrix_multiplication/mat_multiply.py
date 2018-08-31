ab5dcacacad9656e7edc869e6a504a59f54a1b04
Fan Yang <fanyang9@illinois.edu>
2018-08-30 22:02:27 -0400

    modified matrix_multiplication

--- matrix_multiplication/mat_multiply.py
+++ matrix_multiplication/mat_multiply.py
@@ -21,34 +21,13 @@ def matrix_multiply(name_txt_file1, name_txt_file2, sc):
     mat = A.zipWithIndex().cartesian(B.zipWithIndex())
     matMultiply = mat.map(lambda (val1, val2): (val1[1], mul(val1[0][val2[1]], val2[0])))
     matReduce = matMultiply.reduceByKey(lambda a, b: list(map(add, a, b)))
-    return matReduce.map(lambda (key, val): val).collect()
+    return matReduce.map(lambda (key, val): val)
 
 
 if __name__ == '__main__':
-    try:
-        f = open('f1.txt', 'r')
-        f.close()
-        pass
-    except Exception:
-        mat1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-        file = open('f1.txt', 'a')
-        for i in mat1:
-            x = ','.join([str(j) for j in i])
-            file.write(str(x) + '\n')
-        file.close()
-    try:
-        f = open('f2.txt', 'r')
-        f.close()
-        pass
-    except Exception:
-        mat2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-        file = open('f2.txt', 'a')
-        for i in mat2:
-            x = ','.join([str(j) for j in i])
-            file.write(str(x) + '\n')
-        file.close()
     sc = SparkContext()
-    out_mat = matrix_multiply('f1.txt', 'f2.txt', sc)
+    out_mat = matrix_multiply('A.txt', 'B.txt', sc).collect()
+    print(out_mat)
     file = open('result.txt', 'w')
     for i in out_mat:
         file.write(','.join([str(x) for x in i]) + '\n')
